77ba23239c62a0c7cbc9d4cc78ca1da3e70ac67d
uniquehimanshu <67590988+uniquehimanshu@users.noreply.github.com>
2021-01-22 12:39:35 +0530

    Add files via upload

---
+++ day3/int_all_size.py
@@ -0,0 +1,16 @@
+a=int(input())
+b=int(input())
+c=int(input())
+d=int(input())
+
+e=pow(a,b)
+f=pow(c,d)
+g=e+f
+
+
+print(g)
+
+
+
+
+
---
+++ day3/power_mod_power.py
@@ -0,0 +1,7 @@
+a=int(input())
+b=int(input())
+c=int(input())
+
+d=pow(a,b)
+print(d)
+print(d%c)
